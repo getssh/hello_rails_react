f56af0adf6880f6244a12dba53161a13c8f08a60
getssh <gtamrat33@gmail.com>
2023-07-27 16:25:11 +0300

    Add greeting route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :messages
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/random_greeting', to: 'messages#random_greeting'
   # Defines the root path route ("/")
   # root "articles#index"
 end
