098e00be7d77dcebf6b4493f6b081cedcadc83c9
getssh <gtamrat33@gmail.com>
2023-07-27 22:04:12 +0300

    Set messages index as a root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/random_greeting', to: 'messages#random_greeting'
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "messages#index"
 end
